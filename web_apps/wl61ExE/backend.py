f9cbc0b1d13cc964ddc004ac7dda167944ec9adf
admin <matt@dataiku.com>
2020-09-13 07:09:37 +0800

    Saved web app upload-files (WEBAPPUPLOAD2.wl61ExE)

--- web_apps/wl61ExE/backend.py
+++ web_apps/wl61ExE/backend.py
@@ -11,13 +11,13 @@ default_file_extension = ""
 
 client = dataiku.api_client()
 
-global archive_folder, tracking_folder, default_file_extension, file_list
+
 var = dataiku.get_custom_variables()
 archive_folder = var["file_list"] 
 tracking_folder = var["tracking_folder"] 
 default_file_extension = var["default_file_extension"] 
 file_list = var["file_list"] 
-
+global archive_folder, tracking_folder, default_file_extension, file_list
 
 @app.route('/load_params')
 def load_params():
