1f0abedf543574b4319dfc77fa09348873e9b643
admin <matt@dataiku.com>
2020-09-13 23:57:12 +0800

    Saved recipe: WEBAPPUPLOAD2.compute_clear_folder

--- recipes/compute_clear_folder.py
+++ recipes/compute_clear_folder.py
@@ -5,11 +5,8 @@ from dataiku import pandasutils as pdu
 
 # Read recipe inputs
 archived_folder = dataiku.Folder("vOv1eTkv", ignore_flow=True)
-archived_folder_info = archived_folder.get_info()
 tracking_folder = dataiku.Folder("3zAYHyFW",  ignore_flow=True)
-tracking_folder_info = tracking_folder.get_info()
 files_folder = dataiku.Folder("bDGv9Em8", ignore_flow=True)
-files_folder_info = files_folder.get_info()
 
 
 files_folder.clear()
