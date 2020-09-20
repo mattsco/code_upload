0ae57ac36659a1d44e18e0f140148148bb7e4b20
admin <matt@dataiku.com>
2020-09-21 00:38:11 +0800

    Recipe WEBAPPUPLOADCODE.compute_delete_FIF_dataset saved through notebook notebook editor for compute_delete_FIF_dataset

--- recipes/compute_delete_FIF_dataset.py
+++ recipes/compute_delete_FIF_dataset.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -5,13 +6,14 @@ from dataiku import pandasutils as pdu
 
 
 
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
+client = dataiku.api_client()
 
-delete_FIF_dataset_df = ... # Compute a Pandas dataframe to write into delete_FIF_dataset
+p = client.get_project(dataiku.get_custom_variables()["projectKey"])
 
+to_delete = []
+for d in p.list_datasets():
+    if d['name'].startswith("FIF_"):
+        to_delete.append(d['name'])
 
-# Write recipe outputs
-delete_FIF_dataset = dataiku.Dataset("delete_FIF_dataset")
-delete_FIF_dataset.write_with_schema(delete_FIF_dataset_df)
+for d in to_delete:
+    p.get_dataset(d).delete()
