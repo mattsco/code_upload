a3f078c85ded712937af3c604146a74c128221de
admin <matt@dataiku.com>
2020-09-20 22:41:17 +0800

    Saved recipe: WEBAPPUPLOADCODE.compute_pp

--- recipes/compute_pp.py
+++ recipes/compute_pp.py
@@ -1,5 +1,51 @@
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
+
+#UPDATE CONFIG
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+import dataiku
+from dataiku import pandasutils as pdu
+import pandas as pd
+from datetime import datetime
+import dateutil.relativedelta
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+mydataset = dataiku.Dataset("config_editable")
+df = mydataset.get_dataframe()
+
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+col_file_name = df.columns[1]
+col_extension = df.columns[2]
+col_mandatory = df.columns[3]
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+c = dataiku.api_client()
+p = c.get_project(dataiku.get_custom_variables()["projectKey"])
+v = p.get_variables()
+v["standard"]["file_list"] = list(df[col_file_name].values)
+v["standard"]["extension"] = list(df[col_extension].values)
+v["standard"]["mandatory"] = list(df[col_mandatory].values)
+p.set_variables(v)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+today = str(datetime.today()).split()[0]
+d = datetime.strptime(today, "%Y-%m-%d")
+
+l_month = []
+for i in range(6):
+    d2 = datetime.today() - dateutil.relativedelta.relativedelta(months=i)
+    l_month.append(str(d2)[:7])
+
+v["standard"]["list_month"] = l_month
+
+d2 = datetime.today() - dateutil.relativedelta.relativedelta(months=1)
+v["standard"]["previous_month"] = str(d2)[:7]
+
+p.set_variables(v)
+
+
+
+#Run 
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
@@ -74,3 +120,5 @@ for i, row in df.iterrows():
     file_stream = archived_folder.get_download_stream(file_name)
     output_folder.upload_stream(file_name, file_stream)
     print("File %s uploaded for %s"%(file_name, file_type))
+    
+    
