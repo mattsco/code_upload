7bc013c8c642be526b57962114b5424ad45229bc
admin <matt@dataiku.com>
2020-09-20 22:32:50 +0800

    Saved web app upload-files (WEBAPPUPLOADCODE.wl61ExE)

--- web_apps/wl61ExE/backend.py
+++ web_apps/wl61ExE/backend.py
@@ -24,7 +24,7 @@ def upload_to_dss():
         return json.dumps({"status":"No file sent to backend"})
     initial_filename = f.filename
     extension = os.path.splitext(initial_filename)[1]
-    if extension != extension_dict[selected_file]:
+    if "."+extension != extension_dict[selected_file]:
         return json.dumps({"status":"Extension must be '%s', '%s' was found"%(extension_dict[selected_file], extension)})
 
     
