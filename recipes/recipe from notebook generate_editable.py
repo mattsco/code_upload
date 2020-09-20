e4060bb007f26830282b4630ceeb2159e6bde483
admin <matt@dataiku.com>
2020-09-20 22:24:32 +0800

    Saved recipe: WEBAPPUPLOADCODE.recipe from notebook generate_editable

--- recipes/recipe from notebook generate_editable.py
+++ recipes/recipe from notebook generate_editable.py
@@ -12,7 +12,7 @@ df = mydataset.get_dataframe()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 col_file_name = df.columns[1]
-col_extention = df.columns[2]
+col_extension = df.columns[2]
 col_mandatory = df.columns[3]
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
@@ -20,7 +20,7 @@ c = dataiku.api_client()
 p = c.get_project(dataiku.get_custom_variables()["projectKey"])
 v = p.get_variables()
 v["standard"]["file_list"] = list(df[col_file_name].values)
-v["standard"]["extention"] = list(df[col_extention].values)
+v["standard"]["extension"] = list(df[col_extension].values)
 v["standard"]["mandatory"] = list(df[col_mandatory].values)
 p.set_variables(v)
 
